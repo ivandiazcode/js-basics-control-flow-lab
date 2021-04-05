92effd833a81be55843c5eaf4b83faabd7f8c14d
ivan <daniel.iddv@hotmail.com>
2021-04-04 21:05:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  if (feet <= 400) {
+    return 'this one is on me!'
+  } else if(1999 < feet && feet <2500) {
+    return 'I will gladly take your thirty bucks.';
+  }else {
+    return "No can do."
+  }
+  }
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
